9b39dc0b09f21ddde0bee8604283cd2c312dd285
Tristan Sloughter <t@crashfast.com>
2019-03-22 12:39:59 -0600

    version bump 0.2.0

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule OpencensusPhoenix.MixProject do
   def project do
     [
       app: :opencensus_phoenix,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
